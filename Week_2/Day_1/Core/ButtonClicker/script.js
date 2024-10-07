6a9128bbf2db8767f81a137f0a6c5b9f27df79d3
med-mokhtar-nasrali <mokhtarbackup03@gmail.com>
2024-10-07 15:45:13 +0000

    ButtonClicker

---
+++ Week_2/Day_1/Core/ButtonClicker/script.js
@@ -0,0 +1,11 @@
+function logout(element){
+        element.innerHTML = "Logout"
+}        
+
+function removeButton(element){
+    element.remove()
+}
+
+function alertLikes(){
+    alert("Ninja was liked")
+}
