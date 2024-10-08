75092da2875311e5b1aa767f917d8bc2ebcc7df9
med-mokhtar-nasrali <mokhtarbackup03@gmail.com>
2024-10-08 18:53:14 +0000

    weather

---
+++ Week_2/Day_2/Practice/DojoWeather/script.js
@@ -0,0 +1,28 @@
+function showAlert(element){
+    alert("Loading weather report...")
+}
+
+function removeE(element){
+    var   element = document.getElementById("me");
+        element.remove() 
+        
+    }
+
+    // function CtoF(degree, type){
+    //     var temp = document.getElementById(degree);
+    //     console.log(temp.innerText);
+    //     var sec = temp.innerText.split("°");
+    //     var result = Number(sec[0])
+    //     console.log(result)
+    //     temp.innerText=Math.floor(result*1.8)
+    // }
+    
+    // function changer(element){
+    // var border = document.querySelector(element).value
+    // console.log(border) 
+    // var array = ["degree1","degree2","degree3","degree4","degree5","degree6","degree7","degree8" ] 
+    //     for(var i = 0 ; i < array.length;i++){
+    //         CtoF(array[i])
+    //     }
+    // }
+
